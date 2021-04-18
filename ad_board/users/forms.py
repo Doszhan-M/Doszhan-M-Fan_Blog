7a5eb370d4ac1c45fe0348fc3ae24d1174acd785
Doszhan <Doszhan.m@mail.ru>
2021-04-18 20:14:50 +0600

    add email service

---
+++ ad_board/users/forms.py
@@ -0,0 +1,78 @@
+from allauth.account.forms import LoginForm, SignupForm, BaseSignupForm
+from django import forms
+from django.forms import ModelForm, CharField, TextInput, EmailInput, FileInput, PasswordInput, EmailField
+from .models import CustomUser
+from board.models import Post
+
+
+class UserForm(ModelForm):
+    """Модельная форма редактировать профиль"""
+
+    class Meta:
+        model = CustomUser
+
+        fields = ['username', 'first_name', 'last_name', 'email', 'photo', ]
+        
+        labels = {'username': 'Логин', 'first_name': 'Имя',
+                    'last_name': 'Фамилия', 'email': 'email', 'photo': 'Аватарка', }
+
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите текст...',
+                'style': 'width:40ch ',
+            }),
+            'first_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите текст...',
+                'style': 'width:40ch',
+            }),
+            'last_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите текст...',
+                'style': 'width:40ch',
+            }),
+            'email': EmailInput(attrs={
+                'multiple class': 'form-control',
+                'style': 'width:40ch',
+            }),
+            'photo': FileInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Выберите рисунок',
+                'style': 'width:40ch',
+            }),
+        }
+
+
+class MyLoginForm(LoginForm):
+    # условие для применения ACCOUNT_FORMS в settings
+    """Переопределить форму вхожа allauth"""
+    def __init__(self, *args, **kwargs):
+        super(MyLoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['login'] = CharField(
+            label=("E-MAIL"), widget=TextInput(attrs={'class': 'form-control', }))
+        self.fields['password'].widget = PasswordInput(
+            attrs={'class': 'form-control', })
+
+
+class MySignupForm(SignupForm):
+    # можно по разному переопределять форму. Так:
+    email = forms.EmailField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                "type": "email",
+                "placeholder": "E-mail address",
+                "autocomplete": "email",
+            }
+        )
+    )
+    # или так:
+    def __init__(self, *args, **kwargs):
+        super(MySignupForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].widget = PasswordInput(
+            attrs={'class': 'form-control', })
+        self.fields['password2'].widget = PasswordInput(
+            attrs={'class': 'form-control', })
