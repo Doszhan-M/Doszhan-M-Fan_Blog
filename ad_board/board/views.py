cfef96cede5f43b8f1c05ae480d219c6afa31d44
Doszhan <Doszhan.m@mail.ru>
2021-05-07 01:55:35 +0600

    add important to img css

--- ad_board/board/views.py
+++ ad_board/board/views.py
@@ -28,7 +28,6 @@ class PostList(FormView, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(self.context_object_name)
         return context
 
     def form_valid(self, form):
